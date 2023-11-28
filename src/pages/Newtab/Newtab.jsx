702e4d306629ccd5f1dfd87fe48786e227730c9f
Kaiwen Zheng <kaiwen.zheng1939@gmail.com>
2023-11-27 23:17:59 -0500

    working on todo list component

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useState } from 'react';
 import './Newtab.css';
 import './Newtab.scss';
 
 const Newtab = () => {
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
         </p>
