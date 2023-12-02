7672805bcb0e57e3d31b2a956d27707f9ee51fa7
Kaiwen Zheng <kaiwen.zheng1939@gmail.com>
2023-12-01 22:19:00 -0500

    new tab todo list working

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -3,20 +3,55 @@ import './Newtab.css';
 import './Newtab.scss';
 
 const Newtab = () => {
-  const [todoList, setTodoList] = useState([]);
-  const handleAdd = () => {
-    console.log("add");
-  }
-  
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [editingIndex, setEditingIndex] = useState(-1);
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      if (editingIndex === -1) {
+        setTodos([...todos, newTodo]);
+      } else {
+        todos[editingIndex] = newTodo;
+        setTodos([...todos]);
+        setEditingIndex(-1);
+      }
+      setNewTodo('');
+    }
+  };
+
+  const editTodo = (index) => {
+    setNewTodo(todos[index]);
+    setEditingIndex(index);
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
+    <div className="todo-container">
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>{editingIndex === -1 ? 'Add' : 'Update'}</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => editTodo(index)}>Edit</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
