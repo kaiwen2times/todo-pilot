2f8adc3c26dd5a9613d672a396589f6bd972509b
Kaiwen Zheng <kaiwen.zheng1939@gmail.com>
2023-12-13 22:40:25 -0500

    create option page

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Options.css';
 
 interface Props {
@@ -6,7 +6,36 @@ interface Props {
 }
 
 const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+  const [blockedUrls, setBlockedUrls] = useState([])
+
+  useEffect(() => {
+    chrome.storage.local.get(['blockedUrls']).then((results) => {
+      if (results && results.blockedUrls) {
+        setBlockedUrls(results.blockedUrls)
+      }
+    });
+  }, []);
+
+  return (
+    <div className="settings-container">
+      <h1>Settings</h1>
+      <div className="setting-item">
+        <label>Username</label>
+        <input type="text" placeholder="Enter your username"/>
+      </div>
+      <div className="setting-item">
+        <label>Email</label>
+        <input type="email" placeholder="Enter your email"/>
+      </div>
+      <div className="setting-item">
+        <label>Enable Notifications</label>
+        <input type="checkbox"/>
+      </div>
+      <div className="setting-item">
+        <button>Save Changes</button>
+      </div>
+    </div>
+  );
 };
 
 export default Options;
