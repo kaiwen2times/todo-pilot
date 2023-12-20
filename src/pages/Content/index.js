c4b717ffd34f568ac236ae53048336a6446c4f17
Kaiwen Zheng <kaiwen.zheng1939@gmail.com>
2023-12-19 22:43:43 -0500

    working on modify page content

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,6 @@
-import { printLine } from './modules/print';
-
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
+chrome.storage.local.get(['blockUrlList']).then((results) => {
+  if (results && results.blockUrlList) {
+    blockUrlList.
+  }
+});
+document.documentElement.style.display = 'none';
