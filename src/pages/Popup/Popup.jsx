e2336ee3969a2917bf0c75aa34701a1f6d7a777e
Kaiwen Zheng <kaiwen.zheng1939@gmail.com>
2023-12-25 20:21:10 -0500

    finish popup page

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './Popup.css';
 
 const Popup = () => {
+  const goToOptions = () => {
+    chrome.runtime.openOptionsPage();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>🌟 Getting Started is Easy</p>
+        <p><b>Stay Focused:</b> In Settings, add websites you want to block. We'll keep them off-limits until your to-do list is clear.</p>
+       <button className="go-to-option" onClick={goToOptions}>Go to Settings</button>
       </header>
     </div>
   );
